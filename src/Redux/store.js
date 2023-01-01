70f361259e22e0f0297eaabb5cac361a91d60aa2
Luc <Luc1fer.tms.ig@gmail.com>
2023-01-01 21:05:58 +0530

    1

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,4 @@
-//use this variable to create the redux store.
-//the initial data of the redux store should be {counter: 10}
+
 import {legacy_createStore} from 'redux'
 import { reducer, Reducer } from './reducer';
 const store = legacy_createStore(reducer);
