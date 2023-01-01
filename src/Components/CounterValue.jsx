d5817e8591f720d562ad3c7034866d3522496c52
Luc <Luc1fer.tms.ig@gmail.com>
2023-01-01 21:09:02 +0530

    1

--- src/Components/CounterValue.jsx
+++ src/Components/CounterValue.jsx
@@ -5,12 +5,14 @@ import { store } from '../Redux/store';
 const CounterValue = () => {
   let counter = store.getState();
   const [state, setState] = useState(0);
+
   const forceUpdate = () => {
     setState(pre => pre + 1);
   }
   store.subscribe(() => {
     forceUpdate()
   })
+  
   return <div data-testid="counterValue">{counter.counter}</div>;
 };
 export default CounterValue;
